076a28151287b3f5161806d7505c43ff929b6437
Lindsay Gensinger <lindsay@Lindsays-MacBook.local>
2017-01-16 16:50:56 -0800

    layout of 2 week input for daily hours

--- www/scripts/app.js
+++ www/scripts/app.js
@@ -1,36 +1,220 @@
 // render app
 window.onload = function() {
+        
+    // app
+    var hours = {
+        day: 8,
+        max: 24,
+        total: 80
+    };
     
     // data
     var today = moment();
-    var hours = {
-        day: 8,
-        max: 24
-    };
-    var weekArray = [
-        {
-            name: today.format("dddd"), 
-            id: today.format("YYYY-MM-DD"), 
-            hours: today.day() > 0 && today.day() < 6 ? hours.day : 0
-        }
-    ];
     
-    // create an array of day names
-    for (var i=0; i < 6; i++) {
+    // get current pay period
+    // assumes a pay period begins on an even week number and contains 2 weeks
+    var cWeekNumber = parseInt(moment(today).format("W"));
+    
+    // generate a week of date data
+    function generateWeek(week) {
         
-        // get day added
-        var lastDate = weekArray[i];
-        var newDate = moment(lastDate.id);
-        var dateObj = {
-            name: newDate.add(1, "day").format("dddd"),
-            id: newDate.format("YYYY-MM-DD"),
-            hours: newDate.day() > 0 && newDate.day() < 6 ? hours.day : 0
+        // custom day names
+        var dayNames = {
+            sameDay: "[Today]",
+            nextDay: "[Tomorrow]",
+            nextWeek: "dddd",
+            lastDay: "[Yesterday]",
+            lastWeek: "[Last] dddd",
+            sameElse: "[Last] dddd"
         };
         
-        // add to array
-        weekArray.push(dateObj);
+        // get last day of week 1
+        var day = moment().day("Saturday").week(week);
+        
+        var week = [
+            {
+                name: day.calendar(null, dayNames),
+                nameStandard: day.format("D MMMM"),
+                dateNumber: day.format("D"),
+                id: day.format("YYYY-MM-DD"), 
+                hours: day.day() > 0 && day.day() < 6 ? hours.day : 0
+            }
+        ];
+
+        // create an array of day names
+        for (var i=0; i < 6; i++) {
+
+            // get day added
+            var lastDate = week[i];
+            var newDate = moment(lastDate.id);
+            var dateObj = {
+                name: newDate.subtract(1, "day").calendar(null, dayNames),
+                nameStandard: newDate.format("D MMMM"),
+                dateNumber: newDate.format("D"),
+                id: newDate.format("YYYY-MM-DD"),
+                hours: newDate.day() > 0 && newDate.day() < 6 ? hours.day : 0
+            };
+
+            // add to array
+            week.push(dateObj);
+
+        };
+        
+        return week;
         
     };
+    
+    // generate pay period
+    function generatePayPeriod(week) {
+        
+        var weeks = [week - 1, week];
+        var payPeriod = [];
+        
+        // loop through each pay period week of year
+        for (var i = 0; i < weeks.length; i++) {
+            
+            // generate week
+            var ppWeek = generateWeek(weeks[i]);
+
+            // week
+            var weekObj = {
+                name: "week " + (i + 1),
+                start: ppWeek[ppWeek.length - 1].nameStandard,
+                end: ppWeek[0].nameStandard,
+                dates: ppWeek
+            };
+            
+            // add to pay period
+            payPeriod.push(weekObj);
+            
+        };
+        console.log(payPeriod);
+        return payPeriod;
+        
+    };
+    
+    var ppWeekEnd = cWeekNumber % 2 == 0 ? cWeekNumber + 1 : cWeekNumber;
+    var payPeriod = generatePayPeriod(ppWeekEnd);
+    
+    // lifecycle
+    var logMixin = {
+        _log: function(methodName, args) {
+            console.log(methodName, args);
+        },
+        componentWillUpdte: function() {
+            this._log("componentWillUpdate", arguments);
+        },
+        componentDidUpdate: function() {
+            this._log("componentDidUpdate", arguments);
+        },
+        componentWillMount: function() {
+            this._log("componentWillMount", arguments);
+        },
+        componentDidMount: function() {
+            this._log("componentDidMount", arguments);
+        },
+        componentWillUnmount: function() {
+            this._log("componentWillUnmount", arguments);
+        }
+    };
+    
+    // hours display
+    var displayHours = React.createClass({
+        
+        displayName: "displayHours",
+        
+        // lifecycle
+        mixins: [logMixin],
+        
+        // properties
+        propTypes: {
+            day: React.PropTypes.number.isRequired,
+            week: React.PropTypes.number.isRequired,
+            hours: React.PropTypes.number.isRequired
+        },
+        
+        // render
+        render: function() {
+            
+            return React.DOM.span(
+                                        
+                // attributes
+                {
+                    key: this.props.day,
+                    "data-week": this.props.week,
+                    "data-day": this.props.day
+                },
+
+                // content
+                this.props.hours
+
+            )
+
+        }
+        
+    });
+    
+    // hours input
+    var inputHours = React.createClass({
+        
+        displayName: "inputHours",
+        
+        // lifecycle
+        mixins: [logMixin],
+        
+        //properties
+        propTypes: {
+            max: React.PropTypes.number,
+            min: React.PropTypes.number,
+            dayID: React.PropTypes.string.isRequired,
+            hours: React.PropTypes.number.isRequired,
+            name: React.PropTypes.string
+        },
+        
+        // set default properties
+        getDefaultProps: function() {
+            return {
+                max: hours.max,
+                min: 0
+            };
+        },
+        
+        // render
+        render: function() {
+            
+            return React.DOM.form(
+
+                // attributes
+                null,
+
+                // input
+                React.DOM.input({
+                    min: this.props.min,
+                    max: this.props.max,
+                    type: "number",
+                    id: "select-" + this.props.dayID,
+                    name: "select-" + this.props.dayID,
+                    defaultValue: this.props.hours
+                }),
+
+                // label
+                React.DOM.label(
+
+                    // attributes
+                    {
+                        htmlFor: "select-" + this.props.dayID
+                    }, 
+
+                    // content
+                    this.props.name
+
+                )
+
+            )
+            
+        }
+        
+    });
        
     // create input number component
     var addTime = React.createClass({
@@ -39,88 +223,152 @@ window.onload = function() {
         
         // properties
         propTypes: {
-            weekArray: React.PropTypes.arrayOf(
+            payPeriod: React.PropTypes.arrayOf(
                 React.PropTypes.object
             )
-        },
-        
-        // set default properties
-        getDefaultProps: function() {
-            return {
-                
-            };
         },
         
         // set state
         getInitialState: function() {
             return {
-                data: this.props.weekArray,
+                data: this.props.payPeriod,
                 edit: null
             };
         },
         
-        // listen for input change
-        _numberChange: function(event) {
+        // listen for click on day
+        _showEditor: function(event) {
             this.setState({
-                hours: event.target.value
+                edit: {
+                    week: parseInt(event.target.dataset.week),
+                    day: parseInt(event.target.dataset.day)
+                }
             });
         },
         
         // render
         render: function() {
-            
+                        
             // week wrap
-            return React.DOM.div(
+            return React.DOM.section(
                 
                 // attributes
-                null,
+                {
+                    onClick: this._showEditor
+                },
                 
-                // each day
-                this.state.data.map(function(row, idx) {
+                // each week
+                this.state.data.map(function(week, idxW) {
+                    
+                    // each week
                     return React.DOM.div(
                         
                         // attributes
                         {
-                            key: idx
+                            key: idxW
                         },
-                                                
-                        // input
-                        /*React.DOM.input({
-                            min: 0,
-                            max: hours.max,
-                            type: "number",
-                            name: "select" + row.name,
-                            value: row.hours,
-                            onChange: this._numberChange
-                        }),*/
-
-                        // label
-                        React.DOM.label(
-
-                            // attributes
-                            {
-                                htmlFor: "select" + name
-                            }, 
-
-                            // content
-                            row.name
-
-                        ),
-
-                        // calculation
-                        React.DOM.p(
-
-                            // attributes
-                            null, 
-
-                            // content
-                            (row.hours * 60) + " minutes")
                         
-                    );
+                        // week header
+                        React.DOM.hgroup(
+                            
+                            // attributes
+                            null,
+                            
+                            // content
+                            React.DOM.h1(
+                                
+                                // attributes
+                                null,
+                                
+                                // content
+                                week.name
+                                
+                            ),
+                            
+                            React.DOM.h2(
+                            
+                                // attributes
+                                null,
+
+                                // content
+                                week.start + " \u2015 " + week.end
+
+                            )
+                            
+                        ),
+                        
+                        // each day
+                        React.DOM.div(
+                        
+                            // attributes
+                            null,
+                            
+                            // content
+                            week.dates.map(function(day, idxD) {
+                                
+                                var content = day;
+                                var edit = this.state.edit;
+                                var hoursInput = React.createElement(displayHours, {
+                                    hours: content.hours,
+                                    day: idxD,
+                                    week: idxW
+                                });
                     
-                })
+                                // check if edit is set and
+                                // if the active edit is this day
+                                if (edit && edit.week === idxW && edit.day === idxD) {
+                                    
+                                    // make a form to input the day hours
+                                    hoursInput = React.createElement(inputHours, {
+                                        dayID: day.id,
+                                        hours: day.hours,
+                                        name: day.name
+                                    });
+
+                                };
+                                
+                                return React.DOM.div(
+                                    
+                                    // attributes
+                                    {
+                                        key: idxD
+                                    },
+                                    
+                                    // content
+                                    React.DOM.span(
+                                        
+                                        // attributes
+                                        null,
+                                        
+                                        // content
+                                        content.dateNumber
+                                        
+                                    ),
+                                    
+                                    React.DOM.p(
+                                        
+                                        // attributes
+                                        null,
+                                        
+                                        // content
+                                        content.name
+                                        
+                                    ),
+                                    
+                                    // hours
+                                    hoursInput
+                                    
+                                )
+                                
+                            }, this)
+                            
+                        )
+                        
+                    )
+                    
+                }, this)
                                                  
-            );
+            )
             
         }
         
@@ -129,9 +377,9 @@ window.onload = function() {
     // day component
     var dayComponent = ReactDOM.render(
         React.createElement(addTime ,{
-            weekArray: weekArray
+            payPeriod: payPeriod
         }),
-        document.getElementById("my-app")
+        document.getElementById("app")
     );
     
 };
