8da1a55a83662d96e08b0835c1e9b3e21effd8c9
Lindsay Gensinger <lindsay@Lindsays-MacBook.local>
2017-02-14 16:48:58 -0800

    angle calcs implemented on drag start

--- www/app/radial-bar/arc-component.js
+++ www/app/radial-bar/arc-component.js
@@ -1,4 +1,47 @@
 define(function () {
+    
+    var isSelected = false;
+    
+    var arcStartAngle = 0;
+    var arcInnerRadius = 0;
+    var arcOuterRadius = 0;
+    var anglePoint = 0;
+    var handleRadius = 10;
+    var fullAngle = Math.PI * 2;
+    var maxStep = 24 * 60;
+    var anglePerStep = fullAngle / maxStep;
+    var currentStep = 0;
+    var animationDuration = 50;
+    var addAngle;
+    var currentX;
+    var currentY;
+    
+    function findAngleInDegrees(x, y) {
+        
+        Math.degrees = function(radians) {
+            return radians * 180 / Math.PI;
+        };
+        
+        Math.radians = function(degrees) {
+            return degrees * Math.PI / 180;
+        };     
+        
+        var angleInRadians = Math.atan(y/x);
+        var angleAdjust = x < 0 && y > 0 ? 180 : x < 0 && y < 0 ? 180 : x > 0 && y < 0 ? 360 : 0;
+        var angleInDegrees = Math.degrees(angleInRadians) + angleAdjust;
+        
+        return angleInDegrees;
+        
+    };
+    
+    function svgCursor(event, coords, svg) {
+                
+        coords.x = event.clientX;
+        coords.y = event.clientY;
+
+        return coords.matrixTransform(svg.getScreenCTM().inverse());
+
+    };
        
     // create radial viz
     var arc = React.createClass({
@@ -35,30 +78,43 @@ define(function () {
                 .outerRadius(function(d) { return (((this.props.idx + 1) * 0.1) + spacing) * radius; });
            
         },
-        
-        componentDidUpdate: function() {
 
-            //console.log(this.state.ring.end);
-            //console.log(this.props.ring.end);
-        },
-
-        // drag even starts
+        // drag event starts
         _dragStart: function(event) {
             
             console.log("start drag");
+            
+            // set selection tracking
+            isSelected = true;
                             
             // set selected arc handle
             var handle = event.target;
-            var mouseX = event.clientX;
-            var mouseY = event.clientY;
-            var handleD = handle.getAttribute("d");
             
+            // get local svg
+            var svg = document.getElementsByTagName("svg")[this.state.ring.weekIdx];
+            
+            // create svg point object
+            var point = svg.createSVGPoint();
+            
+            // get svg points from cursor location
+            var svgCoords = svgCursor(event, point, svg);
+            
+            // set initial x,y
+            currentX = svgCoords.x - this.props.radius;
+            currentY = (svgCoords.y - this.props.radius) * -1;
+            
+            //var x = event.clientX - currentX;
+            //var y = event.clientY - currentY;
+            
+            // find angle
+            var angle = findAngleInDegrees(currentX, currentY);
+            console.log(angle);
             // bind mouse move to handle
-            handle.addEventListener("mousemove", this._drag, false);
+            //handle.addEventListener("mousemove", this._drag, false);
             
         },
         
-        // drag handle
+        // drag happening
         _drag: function(event) {
             
             console.log("dragging");
@@ -66,8 +122,16 @@ define(function () {
             // clone data
             var ring = Object.assign({}, this.state.ring);
             
+            // mouse locations
+            var x = event.clientX - currentX;
+            var y = event.clientY - currentY;
+            
+            // find angle
+            var angle = findAngleInDegrees(x, y);
+            console.log(angle);
+            
             // update data based on interaction
-            ring.end = ring.end + 1;
+            //ring.end = ring.end + 1;
             
             // set up ring with key to pass up to parent
             var data = {
@@ -83,6 +147,30 @@ define(function () {
             this.setState({
                 ring: ring
             });
+            
+            // set x,y for next drag
+            currentX = event.clientX;
+            currentY = event.clientY;
+            
+        },
+        
+        // drag event ends
+        _dragEnd: function(event) {
+            
+            if (isSelected) {
+            
+                console.log("drag end");
+
+                // set selected arc handle
+                var handle = event.target;
+
+                // remove listener from handle
+                handle.removeEventListener("mousemove", this._drag, false);
+                
+                // set selection tracking
+                isSelected = false;
+                
+            };
             
         },
         
@@ -119,7 +207,7 @@ define(function () {
                     null
 
                 ),
-                React.DOM.circle({cx: alcMidpoint[0], cy: alcMidpoint[1], r: 10, cursor: "move", onMouseDown: this._dragStart}, null)
+                React.DOM.circle({cx: alcMidpoint[0], cy: alcMidpoint[1], r: handleRadius, cursor: "grab", cursor: "-webkit-grab", onMouseDown: this._dragStart, onMouseUp: this._dragEnd, onMouseOut: this._dragEnd}, null)
                 // handle
                 /*React.DOM.path(
                     
